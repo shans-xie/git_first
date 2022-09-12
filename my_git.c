7be7b140d0b17c8ab90d2ecff6cf7f711e09cef4
shans-xie <shans_xie@outlook.com>
2022-09-12 21:07:53 +0800

    [CHG]修改my_git.c内容

--- my_git.c
+++ my_git.c
@@ -4,5 +4,6 @@ void main(void)
     printf("this is first example\n");
     printf("modify 2022-09-12\n");
     printf("modiyf 2022-09-12 20:46\n");
+    printf("modify 2022-09-12 21:07\n");
     return;
 }
