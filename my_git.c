c2ad71f8cc4d6876a24fe6a253a5023a1af78ae4
shans-xie <shans_xie@outlook.com>
2022-09-12 21:11:39 +0800

    [CHG]修改my_git 内容，增加first记录

--- my_git.c
+++ my_git.c
@@ -2,7 +2,7 @@
 void main(void)
 {
     printf("this is first example\n");
-    printf("modify 2022-09-12\n");
+    printf("modify 2022-09-12 first time\n");
     printf("modiyf 2022-09-12 20:46\n");
     printf("modify 2022-09-12 21:07\n");
     return;
