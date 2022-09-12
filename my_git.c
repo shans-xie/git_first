0911d5283453d8f4fbb447d0f0419af6c8477bd7
shans-xie <shans_xie@outlook.com>
2022-09-12 22:05:32 +0800

    [CHG]conflict9999

--- my_git.c
+++ my_git.c
@@ -4,6 +4,6 @@ void main(void)
     printf("this is first example\n");
     printf("modify 2022-09-12 first time\n");
     printf("modiyf 2022-09-12 20:46\n");
-    printf("modify 2022-09-12 21:07#######test1\n");
+    printf("modify 2022-09-12 21:07######9999#test1\n");
     return;
 }
