2c3b1bbe2c9efc74bbe709645af3ba03edd855f5
shans-xie <shans_xie@outlook.com>
2022-09-13 00:08:21 +0800

    iiiii

--- my_git.c
+++ my_git.c
@@ -4,6 +4,10 @@ void main(void)
     printf("this is first example\n");
     printf("modify 2022-09-12 first time\n");
     printf("modiyf 2022-09-12 20:46\n");
+<<<<<<< Updated upstream
     printf("modify 2022-09-12 21:07######ooooottt9999876000#test1uuuukkkk\n");
+=======
+    printf("modify 2022-09-12 21:07######ooooottt9999876000#test1uuuuiiiiii\n");
+>>>>>>> Stashed changes
     return;
 }
