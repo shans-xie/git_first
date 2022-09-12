87d643afcac5c9e0f4aaf3e9419703c2730d4f27
shans-xie <shans_xie@outlook.com>
2022-09-12 23:15:24 +0800

    [chg]上传tttt的修改

--- my_git.c
+++ my_git.c
@@ -4,6 +4,6 @@ void main(void)
     printf("this is first example\n");
     printf("modify 2022-09-12 first time\n");
     printf("modiyf 2022-09-12 20:46\n");
-    printf("modify 2022-09-12 21:07######11119999876000#test1\n");
+    printf("modify 2022-09-12 21:07######tttt9999876000#test1\n");
     return;
 }
